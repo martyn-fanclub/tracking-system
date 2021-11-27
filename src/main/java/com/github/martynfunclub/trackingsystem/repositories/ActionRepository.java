69c06db9a20946b8952dd6d8901bc10e8a91486b
Андрей Усанов <iambreaddd@mail.ru>
2021-11-28 00:24:10 +0300

    Fix mistake in implementation

--- src/main/java/com/github/martynfunclub/trackingsystem/repositories/ActionRepository.java
+++ src/main/java/com/github/martynfunclub/trackingsystem/repositories/ActionRepository.java
@@ -1,10 +1,11 @@
 package com.github.martynfunclub.trackingsystem.repositories;
 
+import com.github.martynfunclub.trackingsystem.models.Action;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.github.martynfunclub.trackingsystem.models.Production;
 
 @Repository
-public interface ActionRepository extends JpaRepository<Production, Long> {
+public interface ActionRepository extends JpaRepository<Action, Long> {
 }
