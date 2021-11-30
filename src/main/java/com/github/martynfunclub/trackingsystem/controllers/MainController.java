945aa85af99b7019de03718e98415539a65b5aae
Kirill Pobedonostsev <pobedonoscevkirill@gmail.com>
2021-11-30 16:06:16 +0300

    Fix CG

--- src/main/java/com/github/martynfunclub/trackingsystem/controllers/MainController.java
+++ src/main/java/com/github/martynfunclub/trackingsystem/controllers/MainController.java
@@ -4,6 +4,7 @@ import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
