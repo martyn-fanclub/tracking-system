62a3578659a1c40034cc46657a20256760fd17a6
Kirill Pobedonostsev <pobedonoscevkirill@gmail.com>
2021-12-01 23:24:49 +0300

    Implement backend with frontend v3.0.0

--- src/main/java/com/github/martynfunclub/trackingsystem/config/WebSecurityConfig.java
+++ src/main/java/com/github/martynfunclub/trackingsystem/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.github.martynfunclub.trackingsystem.services.UserService;
@@ -25,6 +26,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                .and()
                 .authorizeRequests()
                 //Доступ только для пользователей с ролью Администратор
 //                    .antMatchers("/admin/**").hasRole("ADMIN")
