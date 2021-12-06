0b47288e4e168fd3b6ab183b0ec9b5aa2cb446bc
hella-r-02 <77581078+hella-r-02@users.noreply.github.com>
2021-12-07 02:03:10 +0300

    CU-2033rp2 - fix request in OrderRepository

--- src/main/java/com/github/martynfunclub/trackingsystem/repositories/OrderRepository.java
+++ src/main/java/com/github/martynfunclub/trackingsystem/repositories/OrderRepository.java
@@ -15,8 +15,7 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     Set<Order> findByOrderTypeInAndStatusIsOrderByPriorityDesc(Set<OrderType> orderTypes, Status status);
 
     @Query(value = "SELECT * FROM order_types " +
-            "LEFT JOIN order_types_orders ON (order_types.id = order_types_orders.order_type_id) " +
-            "LEFT JOIN orders ON (order_types_orders.order_type_id = orders.order_type_id) " +
+            "LEFT JOIN orders ON (order_types.id = orders.order_type_id) " +
             "LEFT JOIN productions ON (orders.order_type_id = productions.order_id) " +
             "WHERE orders.STATUS='PENDING' ORDER BY ORDERS.PRIORITY", nativeQuery = true)
     List<Order> findByPlaceAndStatusIsOrderByPriority(@Param("id") Long placeId);
